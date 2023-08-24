b4f76cb688023c486f03a3a4dbc0d80ed2bf9693
WVallejos <guille.vallejosr@gmail.com>
2023-08-24 13:17:04 -0300

    WebServer-HW-MejoraCR

--- back/src/index.js
+++ back/src/index.js
@@ -8,18 +8,29 @@ http.createServer(function(req, res) {
     // const characters = fs.readFileSync(__dirname + '/utils/data.js')
     const {url} = req;
     
+    if(url.includes("/rickandmorty/character/")){
+        const id = url.split('/').at(-1);
+        const character = characters.find((char) => char.id === Number(id))
+        if (character) {
+            res.writeHead(200, { "Content-Type": "application/json"})
+            return res.end(JSON.stringify(character))
+        } else {
+            res.writeHead(404, { "Content-Type": "application/json"})
+            return res.end(JSON.stringify({error: "Character Not Found"}))
+        }
 
-
-    if(req.url === "/rickandmorty/character"){
-        res.writeHead(200, { "Content-Type": "application/json"});
-        res.end(JSON.stringify(characters));
-    } else if(req.url.startsWith("/rickandmorty/character/")){
-        const params = req.url.split('/');
-        const id = Number(params[3])
-        res.writeHead(200, {"Content-Type": 'application/json'})
-        const character = characters[id-1]
-        res.end(JSON.stringify(character))
     }
+
+    // if(req.url === "/rickandmorty/character"){
+    //     res.writeHead(200, { "Content-Type": "application/json"});
+    //     res.end(JSON.stringify(characters));
+    // } else if(req.url.startsWith("/rickandmorty/character/")){
+    //     const params = req.url.split('/');
+    //     const id = Number(params[3])
+    //     res.writeHead(200, {"Content-Type": 'application/json'})
+    //     const character = characters[id-1]
+    //     res.end(JSON.stringify(character))
+    // }
 
 
 
